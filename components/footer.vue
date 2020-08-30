4b72be3512bfdeb2934a73aa55b5ce1c0d096e1e
rafaelbcastilhos <rafaelbcastilhos@gmail.com>
2020-08-30 17:24:37 -0300

    add components

---
+++ components/footer.vue
@@ -0,0 +1,42 @@
+<template>
+	<footer class="footer" v-if="todos.length">
+		<span class="todo-contador"><strong>{{ restantes }}</strong>
+			tarefa<span v-if="restantes > 1">s</span> pendente</span>
+			<ul class="filtros">
+				<li>
+					<nuxt-link to = "/" exact>Todos</nuxt-link>
+				</li>
+				<li>
+					<nuxt-link to = "/ativo">Ativo</nuxt-link>
+				</li>
+				<li>
+					<nuxt-link to = "/completo">Completo</nuxt-link>
+				</li>
+			</ul>
+			<button class="limpar-completo" @click="removeCompleto"
+				v-if="todos.length > restantes">
+				Limpar completos
+			</button>
+	</footer>
+</template>
+
+<script>
+export default {
+	computed: {
+		todos(){
+			return this.$store.getters.todosTodos
+		},
+		ativo(){
+			return this.$store.getters.ativoTodos
+		},
+		restantes(){
+			return this.$store.getters.ativoTodos.length
+		}
+	},
+	methods: {
+		removeCompleto(){
+			this.$store.dispatch('defineTodos', this.ativo)
+		}
+	}
+}
+</script>
---
+++ components/header.vue
@@ -0,0 +1,24 @@
+<template>
+	<header class="header">
+		<h1><span>Minima</span>Lista</h1>
+		<input class="novo-todo" placeholder="O que precisa ser feito?" autofocus v-model="todo" @keyup.enter="adicionaTodo">
+	</header>
+</template>
+
+<script>
+export default {
+	data(){
+		return { todo: '' }
+	},
+	methods: {
+		adicionaTodo(){
+			var valor = this.todo && this.todo.trim()
+			if(valor){
+				this.$store.dispatch('adicionaTodo', { title: valor,
+					completo: this.$route.params.slug === 'completo' })
+				this.todo = ''
+			}
+		}
+	}
+}
+</script>
