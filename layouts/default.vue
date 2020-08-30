124e514f33c6d3fc2da17a021b28174f88d8f399
rafaelbcastilhos <rafaelbcastilhos@gmail.com>
2020-08-30 17:24:57 -0300

    add layouts

---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+	<div>
+		<nuxt/>
+		<footer class="info">
+			<p>Duplo clique para editar uma tarefa.</p>
+		</footer>
+	</div>
+</template>
+
+<style>
+.info {
+	font-size: 13px;
+}
+</style>
